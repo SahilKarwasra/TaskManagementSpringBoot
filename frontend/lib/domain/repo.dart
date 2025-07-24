0eb5febe0a4b24577596a20d4157548b2517ac07
SahilKarwasra <sahilkarwasra11@gmail.com>
2025-07-24 16:52:12 +0530

    1

--- frontend/lib/domain/repo.dart
+++
@@ -1,9 +0,0 @@
-import 'package:frontend/domain/task.dart';
-
-abstract class Repo {
-  Future<List<Task>> getTasks();
-  Future<Task> createTask(Task task);
-  Future<Task> updateTask(String id, Task task);
-  Future deleteTask(String id);
-  Future<String> markAsCompleted(Task task);
-}
